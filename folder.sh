016100c5ab0de49e212d47d6601788bbad8f2f8c
w4rr4n7 <w4rr4m7@gmail.com>
2015-03-25 12:14:08 -0500

    first

---
+++ folder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get -y update && sudo apt-get -y upgrade
+
+mkdir .icons .themes .fonts source bin
+
+sudo apt-get install openbox xserver-xorg xinit terminator vim thunar tint2 geany gmrun htop mc inxi xsettingsd i3lock sudo dkms compton network-manager-gnome xfonts-terminus git curl mpv youtube-dl mediainfo mkvtoolnix alsa-base alsa-tools alsa-tools-gui alsa-utils alsa-oss alsamixergui libalsaplayer0 iceweasel viewnior unp lxappearance lxappearance-obconf librsvg2-bin zenity imagemagick dmz-cursor-theme nitrogen obmenu python-xlib python-wxtools conky volti gimp gimp-plugin-registry inkscape galculator gpick font-manager rxvt-unicode catfish apt-listbugs
+sudo apt-get -y install flashplugin-nonfree
+sudo apt-get -y install gtk3-engines-* ?
+sudo apt-get -y install --no-install-recommends fonts-dejavu fonts-droid ttf-freefont ttf-liberation gdebi gtk2-engines-pixbuf gtk2-engines-murrine gtk2-engines-oxygen gtk2-engines-xfce xfce4-appfinder file-roller
+sudo apt-get -y update
